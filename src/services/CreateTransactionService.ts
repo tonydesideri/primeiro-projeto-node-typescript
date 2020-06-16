8bb70d724b79eb44c885e63a7be5c04b3bbfb493
Tony Desideri <tony.desideri@grupoicts.com.br>
2020-06-16 15:57:50 -0400

    finish

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: CreateTransaction): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Type invalid');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Insufficient funds');
+    }
+
+    const createTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return createTransaction;
   }
 }
 
